593179110aa6fc7054ed2531ad9db9deb995ebcc
ChameshJayamanna <chameshtheekshana@gmail.com>
2024-03-15 01:45:16 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,14 @@ import 'package:cal_flutter/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
